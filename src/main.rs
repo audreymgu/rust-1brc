9c787d28e96523ec05f0130bb0fb29fb4dae355a
Audrey Gu <audreymgu@gmail.com>
2026-04-28 12:06:50 -0400

    Working hashmap implementation!

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,67 @@
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 use std::env;
 use std::fs;
 
+#[derive(Debug)]
+struct StationData {
+    min: f64,
+    max: f64,
+    sum: f64,
+    count: u32,
+}
+
 fn main() {
-    let args:Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     // TODO: handle when arg not received
     let file_path = &args[1];
     read_back(file_path);
 }
 
 fn read_back(arg: &str) {
-    // println!("Your file path is {arg}");
-    
-    let contents = fs::read_to_string(arg)
-        .expect("y no read");
+    let contents = fs::read_to_string(arg).expect("y no read");
 
     let list = format(&contents);
     println!("{:#?}", list);
 }
 
-fn format<'a>(arg: &'a String) -> Vec<&'a str> {
-    let mut places: Vec<&str> = Vec::new();
+fn format<'a>(arg: &'a String) -> HashMap<&'a str, StationData> {
+    // create empty hashmap
+    let mut places: HashMap<&str, StationData> = HashMap::new();
+
+    // get each data point
     let data: Vec<&str> = arg.split('\n').collect();
-    // {:#?} is an option for pretty-printed debug output
-    // println!("{:#?}", data);
-    for &point in data.iter() {
-        for &place in places.iter() {
-            if point.contains(place) {
-                continue;
-            } else {
-                let new_place: &str = point.split(';')
-                    .next()
-                    .unwrap_or("error");
-                places.push(new_place);
+
+    // iterate through all data points
+    for point in data.iter() {
+        // split by semicolon and collect
+        let mut data_pair = point.split(';');
+
+        // operate on iterator
+        let label = data_pair.next().unwrap_or("unknown");
+        let value: f64 = data_pair.next().unwrap_or("0").parse().unwrap_or(0.0);
+
+        // check and update hashmap
+        match places.entry(label) {
+            Entry::Occupied(mut current_station) => {
+                let current_data = current_station.get_mut();
+                if value > current_data.max {
+                    current_data.max = value;
+                }
+                if value < current_data.min {
+                    current_data.min = value;
+                }
+                current_data.count += 1;
+                current_data.sum += value;
+            }
+            Entry::Vacant(empty) => {
+                let new_data = StationData {
+                    min: value,
+                    max: value,
+                    sum: value,
+                    count: 1,
+                };
+                empty.insert(new_data);
             }
         }
     }
@@ -42,12 +72,12 @@ fn format<'a>(arg: &'a String) -> Vec<&'a str> {
 // get file contents DONE
 // split by new-line > Vec DONE
 // for item in vec
-    // check name list
-    // if not in list, create Vec to store value (should this just be a reference?)
+// check name list
+// if not in list, create Vec to store value (should this just be a reference?)
 // for each name list
-    // calculate min?
-        // hold one value in memory and compare, switch out when smaller
-    // calculate max?
-        // hold one value in memory and compare, switch out when larger
-    // calculate average
-        // just... get the average?
+// calculate min?
+// hold one value in memory and compare, switch out when smaller
+// calculate max?
+// hold one value in memory and compare, switch out when larger
+// calculate average
+// just... get the average?
